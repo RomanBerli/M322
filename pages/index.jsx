db12f7962212ecde35e8470a649f56742a8d7b8d
Roman Berli <roman.berli2007@gmail.com>
2024-12-12 08:23:50 +0100

    removed header

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ const Map = dynamic(() => import('../components/Map'), { ssr: false });
 const Home = () => {
     return (
         <div style={{ height: '100vh' }}>
-            <h1>Zoo App Map</h1>
+            
             <Map />
         </div>
     );
