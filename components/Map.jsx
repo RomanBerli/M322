9f971331006422c08342a0895b84050498692c4f
rtinase <dimapuntus@ukr.net>
2024-12-19 08:52:37 +0100

    can see a marker

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic';
+import L from 'leaflet';
 import { useEffect, useState } from 'react';
 
 // Import leaflet CSS dynamically to avoid issues during SSR
@@ -22,16 +23,28 @@ export default function Map() {
     // Dynamically import react-leaflet components
     const MapContainer = dynamic(() => import('react-leaflet').then(mod => mod.MapContainer), { ssr: false });
     const TileLayer = dynamic(() => import('react-leaflet').then(mod => mod.TileLayer), { ssr: false });
-    // const Marker = dynamic(() => import('react-leaflet').then(mod => mod.Marker), { ssr: false });
-    // const Popup = dynamic(() => import('react-leaflet').then(mod => mod.Popup), { ssr: false });
+    const Marker = dynamic(() => import('react-leaflet').then(mod => mod.Marker), { ssr: false });
+    const Popup = dynamic(() => import('react-leaflet').then(mod => mod.Popup), { ssr: false });
 
     const bounds = [
         [47.38336, 8.58034], // Southwest corner
         [47.39036, 8.57047], // Northeast corner
     ];
 
+    let iconOptions = {
+        title: "company title",
+        draggable: false,
+    }
+
 
     const position = [47.38686, 8.57592]; // Default coordinates
+    const customIcon = new L.Icon({
+        iconUrl: '/marker.png',
+        iconSize: [38, 95],
+        iconAnchor: [22, 94],
+        popupAnchor: [-3, -76],
+
+    });
 
     return (
         <MapContainer center={position} zoom={40} style={{ height: '100vh', width: '100%' }} maxBounds={bounds} maxBoundsViscosity={1.0} minZoom={16} maxZoom={18}>
@@ -39,11 +52,11 @@ export default function Map() {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
-            {/* <Marker position={position}>
+            <Marker position={position} icon={customIcon}>
                 <Popup>
                     A sample popup for OpenStreetMap integration.
                 </Popup>
-            </Marker> */}
+            </Marker>
         </MapContainer>
     );
 };
